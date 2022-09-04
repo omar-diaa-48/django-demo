9f1ab07f50f8f164cb60769bd7e11e41ea5a0526
Omar Diaaeldine <omardiaaelwakeel@gmail.com>
2022-09-04 12:04:26 +0200

    add registration success template

--- meetups/views.py
+++ meetups/views.py
@@ -1,7 +1,7 @@
 from cmath import log
 from http.client import HTTPException
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from meetups.forms import RegistrationForm
 
@@ -17,5 +17,18 @@ def index(request):
 
 def meetup_details(request, meetup_slug):
 	selected_meetup = Meetup.objects.get(slug=meetup_slug)
-	registration_form = RegistrationForm()
+	
+	if(request.method == 'GET'):
+		registration_form = RegistrationForm()
+	else:
+		registration_form = RegistrationForm(request.POST)
+		if registration_form.is_valid():
+			participant = registration_form.save()
+			selected_meetup.participants.add(participant)
+			return redirect('confirm-registration')
+
+
 	return render(request, 'meetups/meetup-details.html', {'selected_meetup':selected_meetup, 'form':registration_form})
+
+def confirm_registration(request):
+	return render(request, 'meetups/registration-success.html')
